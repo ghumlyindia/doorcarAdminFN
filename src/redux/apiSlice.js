3bdb9be0ba9e543a534f55cfac92e7c2ba55e4e6
Your Name <jetendrasinghratore1@gmail.com>
2026-02-17 18:18:29 +0530

    render link added

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -4,7 +4,7 @@ export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
         // baseUrl: 'http://localhost:5000/api',
-        baseUrl: 'https://doorcarbn.up.railway.app/api',
+        baseUrl: 'https://doorcarbn.onrender.com/api',
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('token');
             if (token) {
